6acd109b0660f04ed673e4a1644ef438cc0ffd12
Marlowe Recht <recht.marlowe@gmail.com>
2023-04-27 16:20:33 -0400

    small change to title and about

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,8 +8,8 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Marlowe`,
-    siteTitleAlt: `view my portfolio`,
+    siteTitle: `Marlowe Recht Portfolio`,
+    siteTitleAlt: `hi im marlowe`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
